cc1c95d3119fce1e91a462303a7f512775f08f3d
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2023-04-23 00:04:37 +0200

    refactor: Rename project throughout project files
    automatically rename project when used as template #91
    
    This commit renames the myproject string to a new project name (Feel++ Project) throughout files in the project using the rename.sh script. It also replaces the hard-coded value "myapp" with the new app name determined by the user input. Additionally, parameter expansion is now used to calculate the project name based on user input, and small improvements were made to the create_short_name() and create_title() functions.

--- rename.sh
+++ rename.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+# This script is used to rename the project from myproject to something else.
+
+create_name(){
+    string=$1
+    # Use parameter expansion to get the second part of the string
+    second_part="${string#*/}"
+    echo "$second_part"
+}
 create_short_name() {
     name=$1
     # Convert the name to lowercase and remove spaces
@@ -16,25 +24,32 @@ create_short_name() {
     shorter_name="${other_words}"
 
     # Print the shorter name
-    return "$shorter_name"
+    echo "$shorter_name"
 }
 create_title() {
     name=$1
-    # Convert the name to lowercase and remove spaces
-    title_name=$(echo "$name" | tr '[:lower:]' '[:upper:]' | sed 's/-/ /g')
+# Convert the name to lowercase and remove spaces
+short_name=$(echo "$name" | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
+
+# Replace hyphens with spaces
+short_name=${short_name//-/ }
+
+# Use parameter expansion to uppercase the first letter of each word
+uppercase_name=$(echo "$short_name" | awk '{for(i=1;i<=NF;i++)sub(/./,toupper(substr($i,1,1)),$i)}1')
+
 
     # Print the shorter name
-    return "$title_name"
+    echo "$uppercase_name"
 }
 
 # This script is used to rename the project from myproject to something else.
 if [ -z "$1" ]; then
     echo "Usage: $0 <projectname> <projecttitle> <shortprojectname> <appname>"
     exit 1
-elif [ "$1" == "-h" -o "$1" == "--help"]; then
+elif [ "$1" == "-h" -o "$1" == "--help" ]; then
     echo "Usage: $0 <projectname> <projecttitle> <shortprojectname> <appname>"
     exit 1
-elif [ "$1" == "-r"]; then
+elif [ "$1" == "-r" ]; then
     in_default_projectname="project"
     read -p "What is the name of the project ? [$in_default_projectname]: " projectname
     projectname=${projectname:-$in_default_projectname}
@@ -51,9 +66,9 @@ elif [ "$1" == "-r"]; then
     read -p "What is the name of the first application? [$in_default_appname]" appname
     appname=${appname:-$in_default_appname}
 else
-    projectname=$1
-    projecttitle=create_title($projectname)
-    shortprojectname=create_short_name($projectname)
+    projectname=$( create_name $1 )
+    projecttitle=$(create_title ${projectname})
+    shortprojectname=$( create_short_name $projectname )
     appname="app"
 fi
 
@@ -61,7 +76,8 @@ echo "     project name: $projectname"
 echo "    project title: $projecttitle"
 echo "project shortname: $shortprojectname" 
 echo "         app name: $appname"
-if [ "$1" == "-r"]; then
+
+if [ "$1" == "-r" ]; then
     PS3="Do you wish to rename this project?"
     select yn in Yes No
     do
@@ -77,21 +93,21 @@ export projecttitle
 export shortprojectname
 export appname
 
-files=( "README.adoc" "CMakeLists.txt" "src/CMakeLists.txt" "src/.tests.myapp" 
-        "site-dev.yml" "docs/antora.yml" "docs/ROOT/index.adoc" 
+files=( "README.adoc" "CMakeLists.txt" "src/CMakeLists.txt" "src/.tests.laplacian"  "src/.tests.toolbox"
+        "docs/site.yml" "docs/antora.yml" "docs/modules/ROOT/pages/index.adoc" 
         ".github/workflows/ci.yml" ".github/workflows/release.yml" )
 for i in "${files[@]}"
 do
     echo "processing renaming in $i ...."
-    perl -077pi.bak -e 's/myproject/$ENV{'projectname'}/sg' $i
-    perl -077pi.bak -e 's/MyProject/$ENV{'projecttitle'}/sg' $i
-    if test -z "$shortprojectname"; then
-        perl -077pi.bak -e 's/set\\(PROJECT_SHORTNAME \"p\"\\)/set(PROJECT_SHORTNAME "$ENV{'shortprojectname'}")/sg' $i
+    perl -077pi.bak -e 's/feelpp-project/$ENV{'projectname'}/sg' $i
+    perl -077pi.bak -e 's/Feel++ Project/$ENV{'projecttitle'}/sg' $i
+    if ! test -z "$shortprojectname"; then
+        echo "shortprojectname is not empty"
+        perl -077pi.bak -e 's/set\\(PROJECT_SHORTNAME \"myp\"\\)/set(PROJECT_SHORTNAME "$ENV{'shortprojectname'}")/sg' $i
     fi    
     perl -077pi.bak -e 's/myapp/$ENV{'appname'}/sg' $i
 done
 
-git mv src/.tests.myapp  src/.tests.$appname
 
 
 
